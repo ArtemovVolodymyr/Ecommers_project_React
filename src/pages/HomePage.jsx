810f4f4e49b1064aa9000b099c36b97efbfa6e83
ArtemovVolodymyr <volodymyr.artemov04@gmail.com>
2025-10-30 00:40:02 +0200

    Use axios to fetch the data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,15 @@
+import axios from 'axios';
 import { Header } from '../components/Header';
 import { products } from '../../starting-code/data/products';
 import './HomePage.css';
 
 
 export function HomePage() {
-  fetch('http://localhost:3000/api/products')
+  axios.get('http://localhost:3000/api/products')
   .then((response) => {
-      return response.json();
-    }).then((data) =>{
-      console.log(data);
-  })
+    response.data
+    console.log(response.data);
+  });
   return (
     <>
       <link rel="icon" type="image/svg+xml" href="../public/home-favicon.png" />
