25e233968fc38a0d2963a1e03b4ab155294c1862
ArtemovVolodymyr <volodymyr.artemov04@gmail.com>
2025-10-30 23:59:19 +0200

    lift cart up and share it between pages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,9 +4,8 @@ import { Header } from '../components/Header';
 import './HomePage.css';
 
 
-export function HomePage() {
+export function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     axios.get('/api/products')
@@ -15,11 +14,7 @@ export function HomePage() {
         setProducts(response.data);
       });
 
-      axios.get('/api/cart-items')
-        .then((response) => {
-         setCart(response.data)
-        });
-      
+
   }, []);
 
   return (
