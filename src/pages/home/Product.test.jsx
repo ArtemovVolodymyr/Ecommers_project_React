55328354fbcdfafcffaa11016562081621df27df
ArtemovVolodymyr <volodymyr.artemov04@gmail.com>
2025-11-03 00:59:34 +0200

    Add Product test

--- src/pages/home/Product.test.jsx
+++ src/pages/home/Product.test.jsx
@@ -10,6 +10,8 @@ vi.mock('axios');
 describe('Product component', () => {
     let product;
     let loadCart;
+    let user;
+
 
     beforeEach(() => {
         product = {
@@ -25,6 +27,8 @@ describe('Product component', () => {
         };
 
         loadCart = vi.fn()
+        user = userEvent.setup();
+
     });
 
 
@@ -64,7 +68,6 @@ describe('Product component', () => {
 
         render(<Product product={product} loadCart={loadCart} />);
 
-        const user = userEvent.setup();
         const addToCartButton = screen.getByTestId('add-to-cart-button');
         await user.click(addToCartButton);
 
@@ -76,6 +79,28 @@ describe('Product component', () => {
             }
         );
 
+        expect(loadCart).toHaveBeenCalled();
+    });
+
+    it('selects a quantity', async () => {
+        render(<Product product={product} loadCart={loadCart} />);
+
+        const quantitySelector = screen.getByTestId('product-quantity-selector');
+        expect(quantitySelector).toHaveValue('1');
+
+        await user.selectOptions(quantitySelector, '3');
+        expect(quantitySelector).toHaveValue('3');
+
+        const addToCartButton = screen.getByTestId('add-to-cart-button');
+        await user.click(addToCartButton);
+
+        expect(axios.post).toHaveBeenCalledWith(
+            '/api/cart-items',
+            {
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 3
+            }
+        );
         expect(loadCart).toHaveBeenCalled();
     });
 });
